7b5f950763f9a8e666685763c8bdadff67291ef2
Christian Menschel <christian@tapwork.de>
2021-01-29 18:22:06 +0100

    Some iOS improvements
    - Dispatch on main thread
    - Pass nil for viewName when NSNull in RN options
    - Use current ViewName if missing in RN options
    - Clean up setting user properties

--- ios/RNInstana.m
+++ ios/RNInstana.m
@@ -1,11 +1,6 @@
 #import "RNInstana.h"
 @import InstanaAgent;
 
-// User
-static NSString *currentUserId = @"";
-static NSString *currentUserEmail = @"";
-static NSString *currentUserName = @"";
-
 // Error
 static NSString *const kErrorDomain = @"com.instana.instana-agent-react-native";
 static NSInteger const kErrorDomainCodeWrongRegex = -1;
@@ -30,80 +25,104 @@ RCT_EXPORT_METHOD(setup:(nonnull NSString *)key reportingUrl:(nonnull NSString *
 
 RCT_EXPORT_METHOD(setView:(nonnull NSString *)viewName)
 {
-    [Instana setViewWithName: viewName];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [Instana setViewWithName: viewName];
+    });
 }
 
 RCT_EXPORT_METHOD(setUserID:(nonnull NSString *)userID)
 {
-    currentUserId = userID;
-    [self updateUser];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [Instana setUserWithId:userID];
+    });
 }
 
 RCT_EXPORT_METHOD(setUserName:(nonnull NSString *)userName)
 {
-    currentUserName = userName;
-    [self updateUser];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [Instana setUserWithName:userName];
+    });
 }
 
 RCT_EXPORT_METHOD(setUserEmail:(nonnull NSString *)userEmail)
 {
-    currentUserEmail = userEmail;
-    [self updateUser];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [Instana setUserWithEmail:userEmail];
+    });
 }
 
 RCT_EXPORT_METHOD(setMeta:(nonnull NSString *)key value:(nonnull NSString*)value)
 {
-    [Instana setMetaWithValue:value key:key];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [Instana setMetaWithValue:value key:key];
+    });
 }
 
 RCT_EXPORT_METHOD(reportEvent:(nonnull NSString *)name options:(NSDictionary *)options)
 {
-    // We use NSString for the startTime and duration because ObjC does not allow us to send optional NSInteger as parameter
-    NSString *startTime = [[options objectForKey: kCustomEventStartTimeKey] stringValue];
-    NSString *duration = [[options objectForKey: kCustomEventDurationKey] stringValue];
-    NSString *viewName = [options objectForKey: kCustomEventViewNameKey];
-    NSDictionary <NSString*, NSString*> *meta = [options objectForKey: kCustomEventMetaNameKey];
-    NSString *backendTracingID = [options objectForKey: kCustomEventBackendTracingIDNameKey];
-    [Instana reportEventWithName:name timestamp:startTime duration:duration backendTracingID:backendTracingID error:nil meta:meta viewName:viewName];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        // We use NSString for the startTime and duration because ObjC does not allow us to send optional NSInteger as parameter
+        long long startTime = [[options objectForKey: kCustomEventStartTimeKey] longLongValue];
+        long long duration = [[options objectForKey: kCustomEventDurationKey] longLongValue];
+        NSDictionary <NSString*, NSString*> *meta = [options objectForKey: kCustomEventMetaNameKey];
+        id viewName = [options objectForKey: kCustomEventViewNameKey];
+        id backendTracingID = [options objectForKey: kCustomEventBackendTracingIDNameKey];
+
+        if (![viewName isKindOfClass:[NSString class]]) {
+            viewName = nil;
+        }
+        if (![backendTracingID isKindOfClass:[NSString class]]) {
+            backendTracingID = nil;
+        }
+        [Instana reportEventWithName:name
+                           timestamp:startTime
+                            duration:duration
+                    backendTracingID:backendTracingID
+                               error:nil
+                                meta:meta
+                            viewName:viewName];
+    });
 }
 
 RCT_EXPORT_METHOD(setIgnoreURLsByRegex:(nonnull NSArray <NSString*>*)regexArray resolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    NSMutableArray <NSRegularExpression*> *regexItems = [@[] mutableCopy];
-    NSMutableArray <NSError*> *errors = [@[] mutableCopy];
-    for (NSString *pattern in regexArray) {
-        NSError *error = nil;
-        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern: pattern options: 0 error: &error];
-        if (error != nil) {
-            [errors addObject:error];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSMutableArray <NSRegularExpression*> *regexItems = [@[] mutableCopy];
+        NSMutableArray <NSError*> *errors = [@[] mutableCopy];
+        for (NSString *pattern in regexArray) {
+            NSError *error = nil;
+            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern: pattern options: 0 error: &error];
+            if (error != nil) {
+                [errors addObject:error];
+            }
+            if (regex != nil) {
+                [regexItems addObject:regex];
+            }
         }
-        if (regex != nil) {
-            [regexItems addObject:regex];
-        }
-    }
 
-    if ([errors count] > 0) {
-        NSDictionary *userinfo = @{@"UnderlyingErrors":errors};
-        NSError *error = [NSError errorWithDomain:kErrorDomain code:kErrorDomainCodeWrongRegex userInfo:userinfo];
-        reject([NSString stringWithFormat:@"%ld", kErrorDomainCodeWrongRegex], @"Wrong RegularExpression pattern", error);
-    } else {
-        [Instana setIgnoreURLsMatching:regexItems];
-        resolve(@"Success");
-    }
+        if ([errors count] > 0) {
+            NSDictionary *userinfo = @{@"UnderlyingErrors":errors};
+            NSError *error = [NSError errorWithDomain:kErrorDomain code:kErrorDomainCodeWrongRegex userInfo:userinfo];
+            reject([NSString stringWithFormat:@"%ld", kErrorDomainCodeWrongRegex], @"Wrong RegularExpression pattern", error);
+        } else {
+            [Instana setIgnoreURLsMatching:regexItems];
+            resolve(@"Success");
+        }
+    });
 }
 
 RCT_EXPORT_METHOD(getSessionID:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    resolve([Instana sessionID]);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        resolve([Instana sessionID]);
+    });
 }
 
 RCT_EXPORT_METHOD(getViewName:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    resolve([Instana viewName]);
-}
-
-- (void)updateUser {
-     [Instana setUserWithId:currentUserId email:currentUserEmail name:currentUserName];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        resolve([Instana viewName]);
+    });
 }
 
 @end
